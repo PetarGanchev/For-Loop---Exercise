594a0aff4909e509bcadbf9d537ade72f3a09fad
Petar Ganchev <PetarGanchev@users.noreply.github.com>
2019-06-19 14:39:20 +0300

    commit 2

--- 02. Half Sum Element/Program.cs
+++ 02. Half Sum Element/Program.cs
@@ -6,7 +6,39 @@ namespace _02._Half_Sum_Element
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int inputN = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < inputN; i++)
+            {
+                int currentInput = int.Parse(Console.ReadLine());
+                sum += currentInput;
+
+                if (i == 0)
+                {
+                   maxNumber = currentInput;
+                }
+                if (currentInput > maxNumber)
+                {
+                    maxNumber = currentInput;
+                }
+
+            }
+            if (sum-maxNumber == maxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                int diff = Math.Abs(maxNumber - (sum - maxNumber));
+                Console.WriteLine($"Diff = {diff}");
+            }
+
+
+
         }
     }
 }
